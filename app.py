38dd8bfa223abfc9a51dd6d697769ffe24cd237b
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-29 22:20:35 +0700

    finish register, login

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from __future__ import print_function # In python 2.7
-import sys
-import os
-
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -11,7 +7,7 @@ from flask_mysqldb import MySQL
 import re
 from datetime import datetime
 
-from helpers import apology, login_required
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -19,6 +15,7 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Session uses filesystem, not signed cookies
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -34,19 +31,20 @@ mysql = MySQL(app)
 
 @app.after_request
 def after_request(response):
-    """responses are not cached"""
+    # Responses are not cached
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-# KAEYA
+
 @app.route("/")
 @login_required
 def index():
-    """Show locked page"""
-    # Gets data from database
-    cursor = mysql.connection.cursor()
+    return render_template("journey.html")
+
+    # Get data from database
+    """ cursor = mysql.connection.cursor()
     j = cursor.execute("SELECT * FROM journeys WHERE date = (SELECT CONVERT(VARCHAR(10), GETDATE(), 105)) AND username = ?", session["username"])
     journey = j[0]["journey"]
     m = cursor.execute("SELECT AVERAGE(mood) AS mood FROM general WHERE username = ? GROUP BY username", session["username"])
@@ -54,7 +52,7 @@ def index():
     p = cursor.execute("SELECT COUNTA(mentioned) AS person FROM general WHERE username = ? GROUP BY username", session["username"])
 
     # If there is no data yet
-    if len(m) >= 1:
+    if m >= 1:
         mood = m[0]["mood"]
         weather = w[0]["weather"]
         mentioned = p[0]["mentioned"]
@@ -63,9 +61,8 @@ def index():
         weather = "None"
         mentioned = "Nobody :( are you lonely?"
 
-    cursor.close()
+    cursor.close() """
 
-    return render_template("index.html", mood=mood, weather=weather, mentioned=mentioned)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -80,14 +77,15 @@ def login():
         cursor = mysql.connection.cursor()
 
         # Query database for username
-        rows = cursor.execute("SELECT password FROM users WHERE username = ?", request.form.get("username"))
+        cursor.execute("SELECT * FROM users WHERE username = %s", (request.form["username"], ))
+        rows = cursor.fetchone()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("Invalid username and/or password", 403)
+        if not rows or not check_password_hash(rows[2], request.form["password"]):
+            return render_template("apology.html", message="Invalid username and/or password")
 
         # Remember which user has logged in
-        session["username"] = rows[0]["username"]
+        session["username"] = rows[1]
         cursor.close()
 
         # Redirect user to home page
@@ -113,30 +111,24 @@ def logout():
 def register():
     """Register user"""
     session.clear()
-    sys.stderr.write("fatal error\n")
-    print >> sys.stderr, 'spam' 
 
     if request.method == "POST":
         cursor = mysql.connection.cursor()
-        rows = cursor.execute("SELECT * FROM users WHERE username = ?", request.form.get("un"))
-        print("Diluc's here\n", file=sys.stderr)
+        un = request.form["un"]
+        pw = request.form['pw']
+        cursor.execute("SELECT username FROM users WHERE username = %s", (un, ))
+        rows = cursor.fetchone()
 
-        # Ensure username is not taken
-        if len(rows) == 1:
-            print("Kaeya's here\n", file=sys.stderr)
-            return apology("Someone has already taken the same username", 400)
+        """ Ensure username is not taken """
+        if rows:
+            return render_template("apology.html", message="Someone has already taken the same username")
 
-        # Ensure passwords match
-        elif request.form.get("pw") != request.form.get("confirm"):
-            print("Ruby's here\n", file=sys.stderr)
-            return apology("Passwords don't match", 400)
+        elif pw != request.form['confirm']:
+            """ Ensure passwords match """
+            return render_template("apology.html", message="Passwords don't match")
 
         else:
-            # https://www.w3schools.com/python/python_regex.asp
-            print(request.form.get("pw"))
-            print(request.form.get("un"))
-            print(request.form.get("confirm"))
-            pw = request.form.get("password")
+            """ https://www.w3schools.com/python/python_regex.asp """
             uppercase = re.search("[A-Z]", pw)
             lowercase = re.search("[a-z]", pw)
             number = re.search("[0-9]", pw)
@@ -144,15 +136,15 @@ def register():
 
             # Check password's criteria
             if not uppercase or not lowercase or not number or not special_character:
-                return apology("Password must contains alphanumeric and special characters", 400)
+                return render_template("apology.html", message="Password must contains alphanumeric and special characters")
 
             # Make a new user
             else:
-                cursor.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
-                request.form.get("un"), generate_password_hash(pw))
-        print("Shira's here\n", file=sys.stderr)
+                cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (un, generate_password_hash(pw)))
+                mysql.connection.commit()
+
         cursor.close()
-        return redirect("/login")
+        return redirect("/")
 
     else:
         return render_template("login.html")
