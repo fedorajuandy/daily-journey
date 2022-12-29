e1ccaf23d9959898b66d4ddff6d2aa162e7bb355
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-29 22:21:02 +0700

    remove dead code

--- helpers.py
+++ helpers.py
@@ -1,15 +1,5 @@
-import os
-import requests
-import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
-
-
-def apology(message, code=400):
-    """Render message as an apology to user."""
-    return render_template("apology.html", message=message, code=code)
-
 
 def login_required(f):
     """
@@ -19,7 +9,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
+        if session.get("username") is None:
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
